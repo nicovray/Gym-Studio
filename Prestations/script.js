1661238f6d4cf7a2c72427f68e497d230c4d4721
Lucie Chauvet <lucie.chauvet10@gmail.com>
2022-03-20 10:47:41 +0100

    ajout media queries + CSS divers + ajout pictos

--- Prestations/script.js
+++ Prestations/script.js
@@ -4,7 +4,7 @@ let i;
 console.log(acc.length)
 
 for (i = 0; i < acc.length; i++) {
-    console.log('plop')
+    
 
   acc[i].addEventListener("click", function() {
     /*  */
@@ -19,5 +19,30 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-  })
+  /* HEADER */
 
+  const hamb = document.querySelector('.hamburger');
+hamb.addEventListener('click', function(e) {
+    e.preventDefault()
+    hamb.classList.toggle("open");
+    nav_ul.classList.toggle("slide");
+    document.body.classList.toggle("noScroll");
+
+});
+
+onscroll = () => {
+    header.classList.add("shadowHeader");
+    footer.classList.add("shadowFooter");
+
+    setTimeout(() => {
+        header.classList.remove("shadowHeader");
+        footer.classList.remove("shadowFooter");
+    }, 1000);
+
+    
+    if (progressBar.style.width == "100%") {
+    progressBar.style.backgroundColor = "green";
+    } else progressBar.style.backgroundColor = "rgb(192, 127, 6)";
+
+  } 
+});
