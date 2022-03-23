39ce6c0bfdeb306cba17e0938a0ade13083836cc
Nicolas AUVRAY <98595218+nicovray@users.noreply.github.com>
2022-03-23 15:40:31 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const message = "Merci ! A bientôt !";
-
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    alert(message);
-  });
