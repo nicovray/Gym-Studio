fc5b5d3cc2d3ed2b5d6bfe90109008812bf72428
misscrazy44 <mehdi.mouilbeau@hotmail.fr>
2022-03-23 13:43:19 +0100

    photo changed + html et css rename

--- script.js
+++ script.js
@@ -16,13 +16,6 @@ onscroll = () => {
         footer.classList.remove("shadowFooter");
     }, 1000);
     
-    const page = document.documentElement; //element HTML
-    let totalHeight = page.scrollHeight; //Height Total of page
-    let visibleHeight = page.clientHeight; //Height visible
-    let scrolling = page.scrollTop; //size of scroll
-    let max = totalHeight - visibleHeight;
-    progressBar.style.width = Math.floor(scrolling / max * 100) + "%"; //width in %
-
     if (progressBar.style.width == "100%")
     progressBar.style.backgroundColor = "green";
     else progressBar.style.backgroundColor = "rgb(192, 127, 6)";
